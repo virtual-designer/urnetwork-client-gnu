e25c90dca192d702515ad63992ccb78bdbc9310c
Ar Rakin <rakinar2@onesoftnet.eu.org>
2025-08-26 23:48:50 +0600

    feat: connection info
    Signed-off-by: Ar Rakin <rakinar2@onesoftnet.eu.org>

--- src/widgets/main.go
+++ src/widgets/main.go
@@ -4,6 +4,7 @@ import (
 	_ "embed"
 	"log"
 	"sort"
+	"os/exec"
 	"github.com/virtual-designer/urnetwork-client-gnu/core"
 	"github.com/diamondburned/gotk4/pkg/gtk/v4"
 	"github.com/diamondburned/gotk4/pkg/gdk/v4"
@@ -22,6 +23,59 @@ type MainView struct {
 	locationList *gtk.ListBox
 	locationListWrapper *gtk.Box
 	connectButton *gtk.Button
+	errorViewLabel *gtk.Label
+	errorViewWrapper *gtk.Box
+
+	connectInfoWrapper *gtk.Box
+	connectStatus *gtk.Label
+}
+
+func (mainView *MainView) onConnectClick() {
+	mainView.connectInfoWrapper.SetVisible(true)
+	mainView.connectStatus.SetLabel("Connecting")
+}
+
+func (mainView *MainView) loadLocations() {
+	errorViewLabel := mainView.errorViewLabel
+	errorViewWrapper := mainView.errorViewWrapper
+	connectButton := mainView.connectButton
+	locationList := mainView.locationList
+	locationListWrapper := mainView.locationListWrapper
+	loadingBox := mainView.loadingBox
+
+	locations, err := core.GetLocations(mainView.authManager.Jwt)
+
+	if err != nil {
+		log.Println("An error has occurred: ", err)
+		errorViewLabel.SetLabel(err.Error())
+
+		loadingBox.SetVisible(false)
+		locationListWrapper.SetVisible(false)
+		errorViewWrapper.SetVisible(true)
+
+		return
+	}
+
+	sort.Slice(locations.Locations, func(i, j int) bool {
+		return locations.Locations[i].Name < locations.Locations[j].Name
+	})
+
+	errorViewWrapper.SetVisible(false)
+	loadingBox.SetVisible(false)
+	locationListWrapper.SetVisible(true)
+
+	for i, location := range locations.Locations {
+		label := gtk.NewLabel(location.Name)
+		locationList.Insert(label, i)
+	}
+
+	locationList.Connect("row-selected", func() {
+		connectButton.SetSensitive(true)
+	})
+
+	connectButton.Connect("clicked", func() {
+		mainView.onConnectClick()
+	})
 }
 
 func NewMainView(authManager *core.AuthManager) *MainView {
@@ -30,19 +84,17 @@ func NewMainView(authManager *core.AuthManager) *MainView {
 	cssProvider := gtk.NewCSSProvider()
 	cssProvider.LoadFromData(mainCSS)
 
-	view := builder.GetObject("MainView").Cast().(*gtk.Box)
-	loadingBox := builder.GetObject("LoadingBox").Cast().(*gtk.Box)
-	locationList := builder.GetObject("LocationList").Cast().(*gtk.ListBox)
-	locationListWrapper := builder.GetObject("LocationListWrapper").Cast().(*gtk.Box)
-	connectButton := builder.GetObject("ConnectButton").Cast().(*gtk.Button)
-
 	mainView := & MainView {
-		Box: view,
+		Box: builder.GetObject("MainView").Cast().(*gtk.Box),
 		authManager: authManager,
-		loadingBox: loadingBox,
-		locationList: locationList,
-		locationListWrapper: locationListWrapper,
-		connectButton: connectButton,
+		loadingBox: builder.GetObject("LoadingBox").Cast().(*gtk.Box),
+		locationList: builder.GetObject("LocationList").Cast().(*gtk.ListBox),
+		locationListWrapper: builder.GetObject("LocationListWrapper").Cast().(*gtk.Box),
+		connectButton: builder.GetObject("ConnectButton").Cast().(*gtk.Button),
+		errorViewLabel: builder.GetObject("ErrorViewLabel").Cast().(*gtk.Label),
+		errorViewWrapper: builder.GetObject("ErrorViewWrapper").Cast().(*gtk.Box),
+		connectInfoWrapper: builder.GetObject("ConnectInfoWrapper").Cast().(*gtk.Box),
+		connectStatus: builder.GetObject("ConnectStatusLabel").Cast().(*gtk.Label),
 	}
 
 	gtk.StyleContextAddProviderForDisplay(
@@ -51,30 +103,6 @@ func NewMainView(authManager *core.AuthManager) *MainView {
 		gtk.STYLE_PROVIDER_PRIORITY_APPLICATION,
 	)
 
-	go func() {
-		locations, err := core.GetLocations(authManager.Jwt)
-
-		if err != nil {
-			log.Println("An error has occurred")
-			return
-		}
-
-		sort.Slice(locations.Locations, func(i, j int) bool {
-			return locations.Locations[i].Name < locations.Locations[j].Name
-		})
-
-		loadingBox.SetVisible(false)
-		locationListWrapper.SetVisible(true)
-
-		for i, location := range locations.Locations {
-			label := gtk.NewLabel(location.Name)
-			locationList.Insert(label, i)
-		}
-
-		locationList.Connect("row-selected", func() {
-			connectButton.SetSensitive(true)
-		})
-	}()
-
+	go mainView.loadLocations()
 	return mainView
 }
